07128bfa11e0472b8eafefeebd51bebd714a537e
Marco De Jesus <Marco_De_Jesus@epam.com>
2021-05-15 09:34:07 -0500

    First phase migration

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 require('./static/js/webslides');
 require('./static/css/webslides.css');
+
+
+// const element = document.getElementById('epam-logo');
+// element.setAttribute('src', '.')
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {loader: 'css-loader'}
+                ]
+            },
+            {
+                test: /\.(png|jpeg|svg)$/,
+                use: [
+                    {loader: 'url-loader'}
+                ]
+            }
+        ]
+    }
+}
